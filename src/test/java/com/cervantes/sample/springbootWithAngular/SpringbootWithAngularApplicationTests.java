47ea0ff52cb6d20ef768500422cd8a7f9c5a85fe
e-cervantes <work.e.cervantes@gmail.com>
2021-02-23 23:32:47 +0800

    Initial commit

---
+++ src/test/java/com/cervantes/sample/springbootWithAngular/SpringbootWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cervantes.sample.springbootWithAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
